7f564af66142a4e259a172c5993b96f2da357e0c
dibdas <dibyandu08das@gmail.com>
2021-05-04 19:34:43 +0530

    modified index.js

--- dist/main.js
+++ dist/main.js
@@ -7,90 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _weather__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./weather */ \"./src/weather.js\");\n\n\n\n(0,_weather__WEBPACK_IMPORTED_MODULE_0__.default)()\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/weather.js":
-/*!************************!*\
-  !*** ./src/weather.js ***!
-  \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\n\nconst weather = () => {\n  const root = document.getElementById('content');\n  root.innerHTML = '';\n  const element = document.createElement('div');\n  element.innerHTML = `\n  <div class=\"bg-dark  weather-container border border-warning\">\n    <div class=\"form-floating mb-3 mt-4 px-5\">\n    \n    <input type=\"text\" class=\"form-control\" placeholder=\"enter the name of city\" >\n  </div>\n  <button class=\"submit\">Submit</button>\n  </div>\n    \n  \n    `;\n  root.appendChild(element);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (weather);\n\n//# sourceURL=webpack://weather-app/./src/weather.js?");
+eval("\n\n\n\n\nconst API ={\n    API_ID:'https://api.openweathermap.org/data/2.5/weather',\n    KEY:'ced4960b4f29616c6327cdcc428f0897'\n}\nconst get_weather=async(city)=>{\n    const awaitdata = await fetch(`${API.API_ID}?q=${city}&units=metric&appid=${API.KEY}`,{mode: 'cors'})\n    .then(function(response){\n        return response.json();\n    })\n    .catch((error) => {\n        console.error('Error:', error);\n      });\n}\n\nconst process_weather_data=(data)=>{\nconst obj_div = document.createElement('div')\n  \n  let obj_string =`humidity:${data.main.humidity}<br>`;\n  obj_div.innerHTML = obj_string;\n  weather-form-content.appendChild(obj_div);\n  \n  \n  obj.pressure = {\n    value: data.main.pressure,\n    unit: 'hPa',\n  };\n  obj.wind = data.wind;\n  [obj.weather] = data.weather;\n  \n\n  console.log(obj)\n  return obj;\n\n\n}\n\n\nconsole.log(get_weather('delhi'))\nget_weather('delhi')\nconsole.log(process_weather_data)\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
